d6ad504b4ff37d82c6d76b0d5aeb9fb904d1f8af
Elliott Brannon <esbrannon@gmail.com>
2024-06-27 13:50:13 -0400

    Added Case Summaries

--- caseGen/generate.js
+++ caseGen/generate.js
@@ -6,7 +6,7 @@ const axios = require('axios'); // To make HTTP requests
 // const OPENAI_API_KEY = 'your-api-key-here';
 
 // Diseases for which we need case summaries
-const diseases = ['Chickenpox', 'Measles', 'Giardia', 'Pertussis', 'Paratyphoid', 'Nesseria Meningitis', 'Hepatitis A','Histoplasmosis','Lyme','EEE'];
+const diseases = ['Chickenpox', 'Measles', 'Giardia', 'Pertussis', 'Paratyphoid', 'Neisseria Meningitis', 'Hepatitis A','Histoplasmosis','Lyme','EEE'];
 
 // Function to create prompt from disease name
 const createPrompt = (disease) => `Generate a detailed case summary for the education of a public health nurse for the following disease: ${disease}. Include information using the follwing headings and sub-headings background (etiology, epidemiology, transmission), patient profile (including demographics, symptoms, testing), subsequent cases (if applicable), Learning Objectives, Actions and Outcomes, Reflection, and Discussion Questions.`;
